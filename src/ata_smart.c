30f0c3c0329afa61a7dd02c8e51940104adfaf76
Baruch Even <baruch@ev-en.org>
2015-09-27 10:03:51 +0300

    Refactor smart attribute matching code

--- src/ata_smart.c
+++ src/ata_smart.c
@@ -1,55 +1,63 @@
 #include "ata_smart.h"
 #include <stdlib.h>
 
-int ata_smart_get_temperature(const ata_smart_attr_t *attrs, int num_attrs, const smart_table_t *table, int *pmin_temp, int *pmax_temp)
+
+static const smart_attr_t *ata_smart_get(const ata_smart_attr_t *attrs, int num_attrs, const smart_table_t *table, const ata_smart_attr_t **pattr, smart_attr_type_e attr_type)
 {
 	const smart_attr_t *attr_info;
 	int i;
 
-	attr_info = smart_attr_for_type(table, SMART_ATTR_TYPE_TEMP);
+	attr_info = smart_attr_for_type(table, attr_type);
 	if (attr_info == NULL)
-		return -1;
+		return NULL;
 
 	for (i = 0; i < num_attrs; i++) {
 		if (attrs[i].id == attr_info->id) {
-			// Temperature is some offset minus the current value, usually
-			int temp = attr_info->offset - attrs[i].value;
-			*pmin_temp = *pmax_temp = -1;
-
-			if (attrs[i].raw) {
-				int min_temp = (attrs[i].raw >> 16) & 0xFFFF;
-				int max_temp = (attrs[i].raw >> 32) & 0xFFFF;
-
-				temp = attrs[i].raw & 0xFFFF;
-
-				if (max_temp >= temp && min_temp <= temp) {
-					*pmin_temp = min_temp;
-					*pmax_temp = max_temp;
-				}
-			}
-			return temp;
+			*pattr = &attrs[i];
+			return attr_info;
 		}
 	}
 
-	return -1;
+	// Attribute not found in disk smart data
+	return NULL;
+}
 
+int ata_smart_get_temperature(const ata_smart_attr_t *attrs, int num_attrs, const smart_table_t *table, int *pmin_temp, int *pmax_temp)
+{
+	const smart_attr_t *attr_info;
+	const ata_smart_attr_t *smart_attr;
+
+	attr_info = ata_smart_get(attrs, num_attrs, table, &smart_attr, SMART_ATTR_TYPE_TEMP);
+	if (attr_info == NULL)
+		return -1;
+
+	// Temperature is some offset minus the current value, usually
+	int temp = attr_info->offset - smart_attr->value;
+	*pmin_temp = *pmax_temp = -1;
+
+	if (smart_attr->raw) {
+		int min_temp = (smart_attr->raw >> 16) & 0xFFFF;
+		int max_temp = (smart_attr->raw >> 32) & 0xFFFF;
+
+		temp = smart_attr->raw & 0xFFFF;
+
+		if (max_temp >= temp && min_temp <= temp) {
+			*pmin_temp = min_temp;
+			*pmax_temp = max_temp;
+		}
+	}
+	return temp;
 }
 
 int ata_smart_get_power_on_hours(const ata_smart_attr_t *attrs, int num_attrs, const smart_table_t *table, int *pminutes)
 {
 	const smart_attr_t *attr_info;
-	int i;
+	const ata_smart_attr_t *smart_attr;
 
-	attr_info = smart_attr_for_type(table, SMART_ATTR_TYPE_POH);
+	attr_info = ata_smart_get(attrs, num_attrs, table, &smart_attr, SMART_ATTR_TYPE_POH);
 	if (attr_info == NULL)
 		return -1;
 
-	for (i = 0; i < num_attrs; i++) {
-		if (attrs[i].id == attr_info->id) {
-			*pminutes = -1;
-			return attrs[i].raw;
-		}
-	}
-
-	return -1;
+	*pminutes = -1;
+	return attrs[i].raw;
 }
